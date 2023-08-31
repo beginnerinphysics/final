12794d5a54bd7e43bd8fd41520605a932f87f9f6
beginnerinphysics <eroha90021@gmail.com>
2023-08-31 23:36:25 +0800

    效果和圖第三頁的波形圖一樣

--- global_counter.sv
+++ global_counter.sv
@@ -1,14 +1,19 @@
 module global_counter#(parameter DWIDTH = 8)(
-    input rst, //shreg[8]會讓rst歸零
+    input rst, //shreg[8]會讓rst歸零 aka HSYNC
     input clk,
-    output [STAGE:0] counter
+    output reg[DWIDTH:0] counter
 );
+reg counterof=0;
 always@(posedge rst or posedge clk)begin
     if(rst) begin
         counter <= 'b0;
+		  counterof <= 'b0;
     end
+    else if(!counterof)begin
+        {counterof,counter} <= {counter+1};
+	 end
     else begin
-        counter <= counter+1;
-    end
+	     counter <= counter;
+	 end
 end
 endmodule
