0819385a21f0eb255c1407f7115fbe5370138ad6
beginnerinphysics <eroha90021@gmail.com>
2023-09-01 00:31:47 +0800

    優化了電路的寫法, 功率應該會降低 速度多一層if但應該不會造成ctp影響

--- global_counter.sv
+++ global_counter.sv
@@ -1,19 +1,21 @@
 module global_counter#(parameter DWIDTH = 8)(
     input rst, //shreg[8]會讓rst歸零 aka HSYNC
     input clk,
-    output reg[DWIDTH:0] counter
+    output reg[DWIDTH-1:0] counter
 );
-reg counterof=0;
-always@(posedge rst or posedge clk)begin
+reg stop=0;
+always@(posedge rst or posedge clk)begin//在always block裡面不會有latch,條件不用補滿
     if(rst) begin
         counter <= 'b0;
-		  counterof <= 'b0;
+		stop <= 'b0;
     end
-    else if(!counterof)begin
-        {counterof,counter} <= {counter+1};
-	 end
-    else begin
-	     counter <= counter;
-	 end
+    else if(!stop)begin//類似mutex
+        counter <= counter+1;
+	end
+end
+always@(negedge counter[DWIDTH-1])begin
+    if(!rst)begin
+        stop<=1;
+    end
 end
 endmodule
