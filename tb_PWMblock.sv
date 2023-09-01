3ed2dd24457aa06b34aea1692fe92dc3336783cb
beginnerinphysics <eroha90021@gmail.com>
2023-09-01 13:21:56 +0800

    驗證PWMblock的tb, 並且引用global_counter且確定正確

---
+++ tb_PWMblock.sv
@@ -0,0 +1,34 @@
+`include "./PWMblock.sv"
+`include "./global_counter.sv"
+`timescale 1ns/1ps
+module tb_PWMblock();
+reg rst;
+reg clk;
+reg hsync;
+wire [7 : 0] counter;
+reg [7:0]data;
+wire out;
+
+initial begin
+    clk = 0;
+    rst = 0;
+  //  counter = 0;
+    hsync = 0;
+    #50 rst = 1;
+    #10 data = 20;
+    #5  hsync <= 1 ;
+        rst <= 0;
+    #400 hsync <= 0;
+        rst <= 1;
+    #1  hsync <= 1;
+        rst <=0;
+ //   #5  hsync= 0 ;
+ //   #5  hsync = 1;
+end
+always #5 clk <= ~clk ;
+global_counter counter_instance(.rst(rst),.clk(clk),.counter(counter));
+PWMblock block_instance(.out(out),.rst(rst),.count(counter),.data(data),.hsync(hsync));
+
+endmodule
+
+
