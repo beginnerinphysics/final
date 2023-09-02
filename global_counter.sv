364a413165160d0a32582bfb7d65b1f077d05aed
beginnerinphysics <eroha90021@gmail.com>
2023-09-03 02:10:39 +0800

    把counter的腳位變成第一秒就數1, 與圖片不同, 但這種數的值才是正確的, 因為PWMblock是count的上一秒值去歸零的, 這樣才能取到正確的值

--- global_counter.sv
+++ global_counter.sv
@@ -3,22 +3,22 @@ module global_counter#(parameter DWIDTH = 8)(
     input clk,
     output reg[DWIDTH-1:0] counter
 );
-reg [1:0]stop=2'b00;//stop[1]表示initial,stop[0]表示數完
+reg stop=1'b0;//stop[1]表示initial,stop[0]表示數完
 always@(posedge rst or posedge clk)begin//在always block裡面不會有latch,條件不用補滿
     if(rst) begin
         counter <= 'b0;
 		stop <= 0;
     end
-    else if(!stop)begin//類似mutex
-        stop[1] <= 1;
-	end
-	else if(!stop[0])begin
+ //   else if(!stop)begin//類似mutex
+ //       stop[1] <= 1;
+//	end
+	else if(!stop)begin
 	    counter <= counter +1;
 	end
 end
 always@(negedge counter[DWIDTH-1])begin
     if(!rst)begin
-        stop[0] <= 1;
+        stop <= 1;
     end
 end
 endmodule
