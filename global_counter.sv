7cc08cc4f359cbf5444c2340ab5dbad0adf753ec
beginnerinphysics <eroha90021@gmail.com>
2023-09-03 17:04:56 +0800

    發現規格的圖畫的不好, count的數字應該在out第一秒的值是1, 而不是0, 所以把globalcounter第一秒的特例改掉

--- global_counter.sv
+++ global_counter.sv
@@ -3,24 +3,30 @@ module global_counter#(parameter DWIDTH = 8)(
     input clk,
     output reg[DWIDTH-1:0] counter
 );
-reg stop=1'b0;//stop[1]表示initial,stop[0]表示數完
+//reg [1:0] stop=2'b00;//stop[1]表示initial,stop[0]表示數完
+reg stop=1'b0;
 always@(posedge rst or posedge clk)begin//在always block裡面不會有latch,條件不用補滿
     if(rst) begin
         counter <= 'b0;
 		stop <= 0;
     end
- //   else if(!stop)begin//類似mutex
- //       stop[1] <= 1;
+//    else if(!stop)begin//類似mutex
+//        stop[1] <= 1;
+        
 //	end
-	else if(!stop&counter==0)begin
-	    counter <= counter +2;
-	end
+
+//	else if(!stop[0]&counter==0)begin
+//	    counter <= counter +2;
+//	end
+
+//	else if(!stop[0])begin
 	else if(!stop)begin
 	    counter <= counter +1;
 	end
 end
 always@(negedge counter[DWIDTH-1])begin
     if(!rst)begin
+    //  stop[0] <= 1;
         stop <= 1;
     end
 end
